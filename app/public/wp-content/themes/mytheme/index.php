34e925c1009719b18b1f449e152adbb09305cbd4
Bludidi <bludidi5@gmail.com>
2024-08-20 16:13:25 +0200

    Initial commit

---
+++ app/public/wp-content/themes/mytheme/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Main template file.
+ * 
+ * @package MyTheme 
+ */
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My Fist WordPress Theme</title>
+ </head>
+ <body>
+  Hello WordPress
+ </body>
+ </html>
